6a2e396a72250e818bb0d11de376c654a4c32b19
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-03-28 20:28:11 +0200

    feat: add bare welcome screen

---
+++ src/screens/auth/Welcome/WelcomeScreen.style.ts
@@ -0,0 +1,6 @@
+import { StyleSheet } from "react-native";
+import { WelcomeScreenStyle } from "./WelcomeScreen.types";
+
+const styles = StyleSheet.create<WelcomeScreenStyle>({});
+
+export default styles;
---
+++ src/screens/auth/Welcome/WelcomeScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./WelcomeScreen.style";
+import { WelcomeScreenProps } from "./WelcomeScreen.types";
+
+const WelcomeScreen = ({}: WelcomeScreenProps) => {
+  return styles;
+};
