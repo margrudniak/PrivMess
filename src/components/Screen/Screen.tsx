17e5ba6fb5850760d504080014ff7bfcb62daa2f
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-02 10:59:11 +0200

    feat: add Screen component

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,6 +1,63 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { ScreenProps } from "./Screen.types";
+import { KeyboardAvoidingView, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { isIos } from "src/utils";
+import {
+  BareScreenProps,
+  KeyboardAwareScreenProps,
+  KeyboardAwareScrollScreenProps,
+  ScreenProps,
+} from "./Screen.types";
 
-export const Screen = ({}: ScreenProps) => {
+const BareScreen = ({ ...props }: BareScreenProps) => {
+  return (
+    <View style={[props.style, props.insetStyle]} {...props}>
+      {props.children}
+    </View>
+  );
+};
+
+const KeyboardAwareScrollScreen = ({
+  ...props
+}: KeyboardAwareScrollScreenProps) => {
   return null;
+};
+
+const KeyboardAwareScreen = ({ ...props }: KeyboardAwareScreenProps) => {
+  console.log("KeyboardAwareScreen invoked", props);
+  return (
+    <KeyboardAvoidingView
+      behavior={isIos ? "padding" : "height"}
+      style={[props.insetStyle, props.style]}
+    >
+      {props.children}
+    </KeyboardAvoidingView>
+  );
+};
+
+export const Screen = (props: ScreenProps) => {
+  const insets = useSafeAreaInsets();
+  const insetStyle = {
+    paddingTop: props.unsafeTop ? 0 : insets.top,
+    paddingBottom: props.unsafeBottom ? 0 : insets.bottom,
+  };
+
+  const renderContent = () => {
+    if (props.keyboardAware) {
+      return <KeyboardAwareScreen {...props} {...{ insetStyle }} />;
+    }
+    if (props.keyboardAwareScroll) {
+      return <KeyboardAwareScrollScreen {...props} {...{ insetStyle }} />;
+    } else {
+      return <BareScreen {...props} {...{ insetStyle }} />;
+    }
+  };
+
+  return (
+    <>
+      <StatusBar style={props.statusBar || "light"} />
+      {renderContent()}
+    </>
+  );
 };
--- src/components/Screen/Screen.types.ts
+++ src/components/Screen/Screen.types.ts
@@ -1,1 +1,25 @@
-export interface ScreenProps {}
+import { StatusBarStyle } from "expo-status-bar";
+import { StyleProp, ViewStyle } from "react-native";
+
+export interface ScreenProps {
+  keyboardAware?: boolean;
+  keyboardAwareScroll?: boolean;
+  unsafeTop?: boolean;
+  unsafeBottom?: boolean;
+  children?: React.ReactNode;
+  statusBar?: StatusBarStyle;
+  keyboardOffset?: number;
+  style?: StyleProp<ViewStyle>;
+}
+
+export interface KeyboardAwareScreenProps extends ScreenProps {
+  insetStyle: StyleProp<ViewStyle>;
+}
+
+export interface KeyboardAwareScrollScreenProps extends ScreenProps {
+  insetStyle: StyleProp<ViewStyle>;
+}
+
+export interface BareScreenProps extends ScreenProps, ViewStyle {
+  insetStyle: StyleProp<ViewStyle>;
+}
