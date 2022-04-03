3b2abbc00d5a72c3ccc3aa755e79a1b97ebb3c8e
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-03 20:15:42 +0200

    feat: add import i18n in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { LogBox } from "react-native";
 import {
   SafeAreaProvider,
   initialWindowMetrics,
 } from "react-native-safe-area-context";
 import { AppNavigator } from "src/navigation";
+import "src/i18n";
 
 const App = () => {
+  LogBox.ignoreAllLogs();
+
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <AppNavigator />
