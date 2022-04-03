92951d7386786097e2b4874dec2fd71c2e69a15d
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-03 20:30:08 +0200

    feat: add basic config on Text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Text as ReactNativeText } from "react-native";
+import i18n from "i18n-js";
 import { TestProps } from "./Text.types";
 import styles from "./Text.style";
 
-export const Test = ({}: TestProps) => {
-  return null;
+export const Test = ({
+  notTranslated,
+  children,
+  text,
+  ...props
+}: TestProps) => {
+  const content = notTranslated ? text : i18n.t(text);
+
+  return (
+    <ReactNativeText {...props} style={styles}>
+      {content}
+    </ReactNativeText>
+  );
 };
--- src/components/Text/Text.types.ts
+++ src/components/Text/Text.types.ts
@@ -1,1 +1,7 @@
-export interface TestProps {}
+import { Scope } from "i18n-js";
+
+export interface TestProps {
+  notTranslated?: boolean;
+  children?: React.ReactNode;
+  text: Scope;
+}
