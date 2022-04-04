7cafa7162e1176ee60149a32a8c39e6473ad860a
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-04 20:16:40 +0200

    feat: add themes basic files

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,7 @@ module.exports = function (api) {
             assets: "./assets",
             src: "./src",
             i18n: "./i18n",
+            themes: "./themes",
           },
         },
       ],
---
+++ src/themes/palette.ts
@@ -0,0 +1,4 @@
+export const palette = {
+  jordyBlue: "#a0b6f7",
+  portica: "#F2F261",
+};
