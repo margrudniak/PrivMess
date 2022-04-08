57c2f15898770aca76fd9a866c591b5dac5d5cae
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-08 20:41:26 +0200

    feat: add text categories

--- src/components/Text/Text.style.ts
+++ src/components/Text/Text.style.ts
@@ -9,10 +9,45 @@ const base: TextStyle = {
 
 interface Style {
   default: TextStyle;
+  h0: TextStyle;
+  h1: TextStyle;
+  h2: TextStyle;
+  h3: TextStyle;
+  h4: TextStyle;
+  p1: TextStyle;
+  p2: TextStyle;
 }
 
 const styles = StyleSheet.create<Style>({
   default: base,
+  h0: {
+    ...base,
+    fontSize: 40,
+  },
+  h1: {
+    ...base,
+    fontSize: 32,
+  },
+  h2: {
+    ...base,
+    fontSize: 26,
+  },
+  h3: {
+    ...base,
+    fontSize: 22,
+  },
+  h4: {
+    ...base,
+    fontSize: 20,
+  },
+  p1: {
+    ...base,
+    fontSize: 13,
+  },
+  p2: {
+    ...base,
+    fontSize: 11,
+  },
 });
 
 export type TextStyleProps = keyof typeof styles;
