79acdec98ce20195cea5a05cf637c7cdea6c9691
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-08 20:42:51 +0200

    feat: add bare SignUpScreen

---
+++ src/screens/auth/SignUp/SignUpScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { SignUpScreenProps } from "./SignUpScreen.types";
+import styles from "./SignUpScreen.style";
+
+export const SignUpScreen = ({}: SignUpScreenProps) => {
+  return null;
+};
