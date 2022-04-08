4380d3697003f09efbba2d4dc47d722221353a18
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-08 20:49:41 +0200

    fix: fix gradient background colors from colors

--- src/screens/auth/Welcome/WelcomeScreen.tsx
+++ src/screens/auth/Welcome/WelcomeScreen.tsx
@@ -3,12 +3,13 @@ import { LinearGradient } from "expo-linear-gradient";
 import { Text, Screen, Button } from "src/components";
 import { WelcomeScreenProps } from "./WelcomeScreen.types";
 import styles from "./WelcomeScreen.style";
+import { color } from "src/themes";
 
 export const WelcomeScreen = ({}: WelcomeScreenProps) => {
   return (
     <>
       <LinearGradient
-        colors={["#404040", "#202020"]}
+        colors={[color.tertiary, color.tertiaryDarker]}
         style={styles.linearBackground}
       />
       <Screen
