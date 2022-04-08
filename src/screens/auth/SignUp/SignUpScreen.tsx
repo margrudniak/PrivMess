f5fa0a193f84bc2880de31042cdacab3fe9f2df0
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-08 21:18:05 +0200

    feat: add LinearGradient on SignUp

--- src/screens/auth/SignUp/SignUpScreen.tsx
+++ src/screens/auth/SignUp/SignUpScreen.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { SignUpScreenProps } from "./SignUpScreen.types";
 import styles from "./SignUpScreen.style";
+import { LinearGradient } from "expo-linear-gradient";
+import { color } from "src/themes";
 
 export const SignUpScreen = ({}: SignUpScreenProps) => {
-  return null;
+  return (
+    <>
+      <LinearGradient
+        colors={[color.tertiary, color.tertiaryDarker]}
+        style={styles.linearBackground}
+      />
+    </>
+  );
 };
