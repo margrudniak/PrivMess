66f3ca9465b28d25851cd26801cecadb4bf8eb14
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-08 21:18:21 +0200

    feat: add LinearGradient on SignIn

--- src/screens/auth/SignIn/SignInScreen.tsx
+++ src/screens/auth/SignIn/SignInScreen.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { SignInScreenProps } from "./SignInScreen.types";
 import styles from "./SignInScreen.style";
+import { LinearGradient } from "expo-linear-gradient";
+import { color } from "src/themes";
 
 export const SignInScreen = ({}: SignInScreenProps) => {
-  return null;
+  return (
+    <>
+      <LinearGradient
+        colors={[color.tertiary, color.tertiaryDarker]}
+        style={styles.linearBackground}
+      />
+    </>
+  );
 };
