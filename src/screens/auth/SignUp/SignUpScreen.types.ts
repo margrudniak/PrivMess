b0ec5b51a6f7146cad4d97cf202026554eb177e6
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-08 21:17:09 +0200

    feat: extend SignUpScreen on StackScreenProps

--- src/screens/auth/SignUp/SignUpScreen.types.ts
+++ src/screens/auth/SignUp/SignUpScreen.types.ts
@@ -1,1 +1,5 @@
-export interface SignUpScreenProps {}
+import { StackScreenProps } from "@react-navigation/stack";
+import { ParamList, Screens } from "src/navigation";
+
+export interface SignUpScreenProps
+  extends StackScreenProps<ParamList, Screens.SignUp> {}
