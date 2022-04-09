e7400c2606f20e019b1e94bdb4702c9efbabb720
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-09 18:19:01 +0200

    feat: adjust TextInput component on react-hooks-form

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TextInput as RNTextInput, View } from "react-native";
+import { useController } from "react-hook-form";
 import { color, palette, PaletteTypes } from "src/themes";
 import { Text } from "../Text/Text";
 import styles from "./TextInput.style";
@@ -8,8 +9,16 @@ import { TextInputProps } from "./TextInput.types";
 export const TextInput = ({
   label,
   notTranslated,
+  name,
+  control,
   ...props
 }: TextInputProps) => {
+  const { field } = useController({
+    control,
+    defaultValue: "",
+    name: label as string,
+  });
+
   return (
     <View style={styles.mainWrapper}>
       <Text
@@ -19,7 +28,12 @@ export const TextInput = ({
         color={palette.white as PaletteTypes}
         {...{ notTranslated }}
       />
-      <RNTextInput style={styles.input} {...props} />
+      <RNTextInput
+        value={field.value}
+        onChangeText={field.onChange}
+        style={styles.input}
+        {...props}
+      />
     </View>
   );
 };
