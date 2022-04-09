d9b5da3d74fea792eadb0b01c6416178ec934f8d
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-09 16:11:31 +0200

    feat: add basic components in SignUpScreen

--- src/screens/auth/SignUp/SignUpScreen.tsx
+++ src/screens/auth/SignUp/SignUpScreen.tsx
@@ -3,14 +3,19 @@ import { SignUpScreenProps } from "./SignUpScreen.types";
 import styles from "./SignUpScreen.style";
 import { LinearGradient } from "expo-linear-gradient";
 import { color } from "src/themes";
+import { Header, Screen } from "src/components";
 
-export const SignUpScreen = ({}: SignUpScreenProps) => {
+export const SignUpScreen = ({ navigation }: SignUpScreenProps) => {
+  const onPressBack = () => navigation.goBack();
   return (
     <>
       <LinearGradient
         colors={[color.tertiary, color.tertiaryDarker]}
         style={styles.linearBackground}
       />
+      <Screen style={{ flex: 1, borderWidth: 1, borderColor: "red" }}>
+        <Header left="back" onPressLeft={onPressBack} />
+      </Screen>
     </>
   );
 };
