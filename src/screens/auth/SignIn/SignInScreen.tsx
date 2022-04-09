d6fa9164a47a08618c821b9ddfad260594f986c2
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-09 18:19:17 +0200

    feat: adjust SignInScreen with react-hook-form

--- src/screens/auth/SignIn/SignInScreen.tsx
+++ src/screens/auth/SignIn/SignInScreen.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Alert } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useForm } from "react-hook-form";
 import { color } from "src/themes";
-import { Screen, Header, TextInput } from "src/components";
+import { Screen, Header, TextInput, Button } from "src/components";
 import { SignInScreenProps } from "./SignInScreen.types";
 import styles from "./SignInScreen.style";
 
 export const SignInScreen = ({ navigation }: SignInScreenProps) => {
+  const { control, handleSubmit } = useForm();
+
   const onPressBack = () => navigation.goBack();
+  const onSubmit = (data) => Alert.alert(JSON.stringify(data));
+
   return (
     <>
       <LinearGradient
@@ -15,8 +21,14 @@ export const SignInScreen = ({ navigation }: SignInScreenProps) => {
       />
       <Screen style={styles.mainWrapper}>
         <Header left="back" onPressLeft={onPressBack} />
-        <TextInput notTranslated label="E-mail" />
-        <TextInput label="form.password" />
+        <TextInput notTranslated label="E-mail" {...{ control }} />
+        <TextInput label="form.password" {...{ control }} />
+        <Button
+          category="secondary"
+          textCategory="h3"
+          text={"form.register"}
+          onPress={handleSubmit(onSubmit)}
+        />
       </Screen>
     </>
   );
