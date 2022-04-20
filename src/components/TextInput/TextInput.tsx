c4ac0f957e5b19fd75aa123515ac986250d3d50c
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-20 19:23:50 +0200

    feat: errors & control handle on TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TextInput as RNTextInput, View } from "react-native";
 import { useController } from "react-hook-form";
 import { color, palette, PaletteTypes } from "src/themes";
@@ -9,8 +9,8 @@ import { TextInputProps } from "./TextInput.types";
 export const TextInput = ({
   label,
   notTranslated,
-  name,
   control,
+  errors,
   ...props
 }: TextInputProps) => {
   const { field } = useController({
@@ -18,6 +18,10 @@ export const TextInput = ({
     defaultValue: "",
     name: label as string,
   });
+
+  useEffect(() => {
+    console.log("errors", errors);
+  }, [errors]);
 
   return (
     <View style={styles.mainWrapper}>
--- src/components/TextInput/TextInput.types.ts
+++ src/components/TextInput/TextInput.types.ts
@@ -1,7 +1,10 @@
 import { TextProps } from "../Text/Text.types";
 import { TextInputProps as RNTextInputProps } from "react-native";
+import { Control, FieldErrors, FieldValues } from "react-hook-form";
 
 export interface TextInputProps extends RNTextInputProps {
   label: TextProps["text"];
   notTranslated?: TextProps["notTranslated"];
+  control: Control<any>;
+  errors: FieldErrors<any>;
 }
