2441d75c96b2d705f7978c84074e5b8dc01b39cf
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-28 21:09:09 +0200

    feat: adjust changes with current db on main file

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import { db } from './src/db';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
+
+db.sequelize.sync();
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
 app.listen(port, () => {
