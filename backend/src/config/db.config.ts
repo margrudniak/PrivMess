97f0fd6bdafc7a64cb358fc7ffd9e7e2566f29ec
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-28 21:05:22 +0200

    feat: prepare config for make db

---
+++ backend/src/config/db.config.ts
@@ -0,0 +1,15 @@
+import { Options } from 'sequelize/types';
+
+export const config: Options = {
+  host: 'localhost',
+  username: 'postgres',
+  password: '123',
+  database: 'testdb',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
