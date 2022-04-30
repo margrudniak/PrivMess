0a2095397c79bc9fce7671f1e5d32b988931e178
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-30 17:23:16 +0200

    fix: adjust changes on main file

--- backend/index.ts
+++ backend/index.ts
@@ -1,30 +1,24 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import { db } from './src/db';
-import { createAuthRoutes } from './src/routes';
+import { sequelize } from './src/db';
+import { authRoute } from './src/routes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-(async () => {
-  await db.sequelize.sync({ force: true });
-})();
-
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ info: 'Node.js, Express, and Postgres API' });
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+sequelize.sync({ force: true }).then(() => {
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  });
 });
 
 //routes
-createAuthRoutes(app);
+app.use('/api/auth', authRoute);
