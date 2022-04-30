3085243a97665cc6e7bfaea9349ce15dc3775dde
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-04-30 17:22:52 +0200

    fix: redefine authRoutes by express Router

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,12 +1,8 @@
-import { Express, Request, Response, NextFunction } from 'express';
+import express from 'express';
 import { signin, signup } from '../controllers';
 import { checkIfEmailExist } from '../middleware';
 
-export const createAuthRoutes = (app: Express) => {
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
-    next();
-  });
-  app.post('/api/auth/signup', checkIfEmailExist, signup);
-  app.post('/api/auth/signin', signin);
-};
+export const authRoute = express.Router();
+
+authRoute.post('/signup', checkIfEmailExist, signup);
+authRoute.post('/signin', signin);
