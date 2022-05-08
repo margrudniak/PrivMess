dc4026f7e4b8f59fe29cee96bd954b39af84d1a5
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-05-08 19:18:15 +0200

    feat: add signuout route to route stack

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signin, signup } from '../controllers';
+import { signin, signout, signup } from '../controllers';
 import { checkIfEmailExist } from '../middleware';
 
 export const authRoute = express.Router();
 
 authRoute.post('/signup', checkIfEmailExist, signup);
 authRoute.post('/signin', signin);
+authRoute.post('/signout', signout);
