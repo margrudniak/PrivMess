480f9ef47c09256c69f29403ce488319d507325d
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-05-09 20:27:24 +0200

    feat: add basic handle on DasboardScreen

--- mobile/src/screens/app/Dashboard/DashboardScreen.tsx
+++ mobile/src/screens/app/Dashboard/DashboardScreen.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Button, Screen } from 'src/components';
+import { Screens } from 'src/navigation';
+import { signOutAction } from 'src/redux/slices';
+import { useAppDispatch } from 'src/utils/hooks';
 import styles from './DashboardScreen.style';
 import { DashboardScreenProps } from './DashboardScreen.types';
 
 export const DashboardScreen = ({ navigation }: DashboardScreenProps) => {
-  return null;
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(signOutAction());
+  };
+  return (
+    <Screen style={{ flex: 1, backgroundColor: 'black' }}>
+      <Button notTranslated category="secondary" text="logout" onPress={handleLogout} />
+    </Screen>
+  );
 };
