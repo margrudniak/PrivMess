186489ce2e96dee1cb0d2e20fddd6b2a139d7032
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-05-19 19:02:58 +0200

    feat: add removePost controller

--- backend/src/controllers/post.ts
+++ backend/src/controllers/post.ts
@@ -1,19 +1,31 @@
 import { Request, Response } from 'express';
-import { Post, User } from '../models';
+import { User } from '../models';
 
 export const createPost = async (req: Request, res: Response) => {
   try {
-    const user = await User.findByPk(req.body.id);
+    const user = await User.findByPk(req.body.userId);
     if (!user) {
       return res.status(404).send({ message: 'User Not found.' });
     }
     const post = await user.createPost({
       message: req.body.message,
-      createdAt: new Date()
     });
     if (post) {
       res.status(200).send({ message: 'Post created!' });
     }
+  } catch (error) {
+    console.error('ERROR signup: ', error);
+    res.status(500).send({ message: JSON.parse(JSON.stringify(error.message)) });
+  }
+};
+
+export const removePost = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByPk(req.body.userId);
+    if (!user) {
+      return res.status(404).send({ message: 'User Not found.' });
+    }
+    await user.removePost(req.body.postId).then(() => res.status(200).send({ message: 'Post deleted!' }));
   } catch (error) {
     console.error('ERROR signup: ', error);
     res.status(500).send({ message: error.message });
