64814fec7e19712f44364d75f994925801e66e92
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-05-19 19:03:12 +0200

    feat: add removePost to routes

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,9 +1,8 @@
 import express from 'express';
-// import { createPost } from 'src/controllers';
-// import { verifyToken } from 'src/middleware';
 import { verifyToken } from '../middleware';
-import { createPost } from '../controllers';
+import { createPost, removePost } from '../controllers';
 
 export const postRoute = express.Router();
 
 postRoute.post('/create', verifyToken, createPost);
+postRoute.post('/remove', verifyToken, removePost);
