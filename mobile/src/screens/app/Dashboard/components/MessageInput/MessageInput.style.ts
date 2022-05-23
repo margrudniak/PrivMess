1515627170c95c26502b0b0b3ac4766671214cba
margrudniak <maverickmonkey69@gmail.com>
2022-05-23 20:13:13 +0200

    feat: add MessageInput component

--- mobile/src/screens/app/Dashboard/components/MessageInput/MessageInput.style.ts
+++ mobile/src/screens/app/Dashboard/components/MessageInput/MessageInput.style.ts
@@ -1,7 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ViewStyle } from 'react-native';
+import { color, palette, spacing } from 'src/themes';
 
-interface Style {}
+interface Style {
+  inputWrapper: ViewStyle;
+  input: ViewStyle;
+}
 
-const styles = StyleSheet.create<Style>({});
+const styles = StyleSheet.create<Style>({
+  inputWrapper: {
+    backgroundColor: palette.white,
+    padding: spacing[3],
+    marginVertical: spacing[3],
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: color.tertiaryDarker,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  input: {
+    width: '90%'
+  }
+});
 
 export default styles;
--- mobile/src/screens/app/Dashboard/components/MessageInput/MessageInput.tsx
+++ mobile/src/screens/app/Dashboard/components/MessageInput/MessageInput.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Pressable, TextInput as RNTextInput, View } from 'react-native';
 import { MessageInputProps } from './MessageInput.types';
 import styles from './MessageInput.style';
+import { AntDesign } from '@expo/vector-icons';
 
 export const MessageInput = ({}: MessageInputProps) => {
-  return null;
+  return (
+    <View style={styles.inputWrapper}>
+      <RNTextInput multiline autoCapitalize="none" autoCorrect={false} style={styles.input} />
+      <Pressable>
+        <AntDesign name="rightcircle" size={24} color="black" />
+      </Pressable>
+    </View>
+  );
 };
