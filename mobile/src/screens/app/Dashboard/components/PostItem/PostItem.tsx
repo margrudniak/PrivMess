f8b1c124aadc3a39baa8b3c6a08f47c9ef57c16c
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-06-07 12:34:15 +0200

    feat: add PostItem color static black

--- mobile/src/screens/app/Dashboard/components/PostItem/PostItem.tsx
+++ mobile/src/screens/app/Dashboard/components/PostItem/PostItem.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Text } from 'src/components';
+import { palette } from 'src/themes';
 import styles from './PostItem.style';
 import { PostItemProps } from './PostItem.types';
 
 export const PostItem = ({ value }: PostItemProps) => {
-  return <Text notTranslated textCategory="h2" text={value} />;
+  return <Text notTranslated color={palette.black} textCategory="h2" text={value} />;
 };
