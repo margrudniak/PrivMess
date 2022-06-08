9775f91e893cedcae34fbee998a9b03e51cba822
margrudniak <maverickmonkey69@gmail.com>
2022-06-08 15:10:58 +0200

    feat: pass View wrapper on DismissKeyboard

--- mobile/src/components/DismissKeyboard/DismissKeyboard.tsx
+++ mobile/src/components/DismissKeyboard/DismissKeyboard.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
 
 export const DismissKeyboard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      {children}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>{children}</View>
     </TouchableWithoutFeedback>
   );
 };
