27cfc37f4589712a914e1ae21c6377fc89b89e35
margrudniak <maverickmonkey69@gmail.com>
2022-06-08 15:10:10 +0200

    feat: add wrapper TouchableWithoutFeedback on Posttem

--- mobile/src/screens/app/Dashboard/components/PostItem/PostItem.tsx
+++ mobile/src/screens/app/Dashboard/components/PostItem/PostItem.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
 import { Text } from 'src/components';
 import { palette } from 'src/themes';
 import styles from './PostItem.style';
 import { PostItemProps } from './PostItem.types';
 
 export const PostItem = ({ value }: PostItemProps) => {
-  return <Text notTranslated color={palette.black} textCategory="h2" text={value} />;
+  return (
+    <TouchableWithoutFeedback onPress={() => {}}>
+      <Text notTranslated color={palette.black} textCategory="h2" text={value} />
+    </TouchableWithoutFeedback>
+  );
 };
