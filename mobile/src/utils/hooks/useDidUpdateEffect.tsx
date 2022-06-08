ca7ca2a363ac589e680bb61cce81169d79012a9c
margrudniak <maverickmonkey69@gmail.com>
2022-06-08 15:09:51 +0200

    feat: add useDidUpdateEffect hook

---
+++ mobile/src/utils/hooks/useDidUpdateEffect.tsx
@@ -0,0 +1,12 @@
+import {DependencyList, useEffect, useRef} from 'react';
+
+export const useDidUpdateEffect = (
+  func: () => unknown,
+  deps?: DependencyList,
+) => {
+  const didMount = useRef(false);
+
+  useEffect(() => {
+    didMount.current ? func() : (didMount.current = true);
+  }, deps);
+};
