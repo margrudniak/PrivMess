cb6ec4b50fa25772e8db937fa3fd8bf678b12364
margrudniak <maverickmonkey69@gmail.com>
2022-06-09 19:29:28 +0200

    feat: add styles on PostItem

--- mobile/src/screens/app/Dashboard/components/PostItem/PostItem.tsx
+++ mobile/src/screens/app/Dashboard/components/PostItem/PostItem.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { TouchableWithoutFeedback } from 'react-native';
+import { TouchableWithoutFeedback, View } from 'react-native';
 import { Text } from 'src/components';
-import { palette } from 'src/themes';
+import { color, palette } from 'src/themes';
 import styles from './PostItem.style';
 import { PostItemProps } from './PostItem.types';
 
 export const PostItem = ({ value }: PostItemProps) => {
   return (
     <TouchableWithoutFeedback onPress={() => {}}>
-      <Text notTranslated color={palette.black} textCategory="h2" text={value} />
+      <View
+        style={{
+          margin: 10,
+          borderRadius: 3,
+          borderWidth: 6,
+          borderColor: color.secondary,
+          alignItems: 'flex-end',
+          backgroundColor: color.secondary
+        }}
+      >
+        <Text notTranslated color={palette.black} textCategory="h2" text={value} />
+      </View>
     </TouchableWithoutFeedback>
   );
 };
